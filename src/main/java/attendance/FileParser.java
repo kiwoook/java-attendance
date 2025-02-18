efd8716222016d7acf1ca8ab43013ce3bd8f2a21
Firedrago95 <yonghwa95@naver.com>
2025-02-18 15:33:10 +0900

    test: 파일 읽기 테스트 작성

---
+++ src/main/java/attendance/FileParser.java
@@ -0,0 +1,39 @@
+package attendance;
+
+import attendance.dto.FileRequestDto;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileParser {
+
+    private final String path;
+
+    public FileParser(String path) {
+        this.path = path;
+    }
+
+    public List<FileRequestDto> read() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            List<FileRequestDto> fileRequestDtos = new ArrayList<>();
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                String[] split = line.split(",");
+                String nickName = split[0];
+                LocalDateTime dateTime = LocalDateTime.parse(split[1], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+                fileRequestDtos.add(new FileRequestDto(nickName, dateTime));
+            }
+            return fileRequestDtos;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+}
--- src/test/java/attendance/FileParserTest.java
+++ src/test/java/attendance/FileParserTest.java
@@ -2,15 +2,19 @@ package attendance;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
+import attendance.dto.FileRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class FileReaderTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FileParserTest {
 
     @Test
     @DisplayName("파일을 읽고 dto로 반환한다")
     void readFile() {
-        FileReader reader = new FileReader("src/test/resources/testAttendances.csv");
+        FileParser reader = new FileParser("src/test/java/resources/testAttendances.csv");
         List<FileRequestDto> expected = List.of(
                 new FileRequestDto("쿠키", LocalDateTime.of(2024,12,13,10,8)),
                 new FileRequestDto("빙봉", LocalDateTime.of(2024,12,13,10,7)),
