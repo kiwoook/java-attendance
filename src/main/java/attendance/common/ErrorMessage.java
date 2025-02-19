bab08bcf0b0751e6d3ea9aa048a08a7c2275d897
7i.w__k <tizmfns1218@Naver.com>
2025-02-19 20:39:49 +0900

    feat: 계층 구현

--- src/main/java/attendance/common/ErrorMessage.java
+++ src/main/java/attendance/common/ErrorMessage.java
@@ -1,13 +1,15 @@
 package attendance.common;
 
+import attendance.utils.DateConverter;
+import java.time.LocalDate;
+
 public enum ErrorMessage {
     INVALID_OPTION_INPUT("기능은 1,2,3,4,Q만 입력 가능합니다"),
     NOT_OPEN_TIME("운영시간은 08 ~ 23시 까지 입니다."),
     NO_NAME("등록되지 않은 닉네임입니다."),
     NO_ATTENDANCE_RECORD("수정 할 출석 기록을 찾을 수 없습니다."),
     INVALID_DATE("출석기록하는 달이 아닙니다."),
-    NOT_OPEN_DAY("%d은 등교일이 아닙니다.")
-    ;
+    NOT_OPEN_DAY("%s은 등교일이 아닙니다.");
 
     private static final String PREFIX = "[ERROR] ";
 
@@ -17,11 +19,11 @@ public enum ErrorMessage {
         this.message = message;
     }
 
-    public String getMessage() {
-        return PREFIX + message;
+    public static String getFormattedMessage(LocalDate localDate) {
+        return String.format(NOT_OPEN_DAY.getMessage(), DateConverter.convertToString(localDate));
     }
 
-    public static String getFormattedMessage(String date) {
-        return String.format(NOT_OPEN_DAY.getMessage(), date);
+    public String getMessage() {
+        return PREFIX + message;
     }
 }
--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static final String OPTION_INPUT_MESSAGE = "오늘은 %s입니다. 기능을 선택해 주세요.%n" +
         "1. 출석 확인%n" +
         "2. 출석 수정%n" +
@@ -17,11 +19,14 @@ public class InputView {
 
     public Option readOption(LocalDate now) {
         System.out.printf(OPTION_INPUT_MESSAGE, DateConverter.convertToString(now));
-        Scanner scanner = new Scanner(System.in);
         return Option.find(scanner.nextLine());
     }
 
-    public void readNickName() {
+    public String readNickName() {
+        System.out.println();
+        System.out.println("닉네임을 입력해 주세요.");
 
+        return scanner.nextLine();
     }
+
 }
