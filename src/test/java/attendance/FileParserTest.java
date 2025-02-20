e8aa07ab08a2c4078a9f725197f05db5a9ca9359
Firedrago95 <yonghwa95@naver.com>
2025-02-20 15:27:47 +0900

    refactor: fileParser 메서드 분리

--- src/test/java/attendance/FileParserTest.java
+++ src/test/java/attendance/FileParserTest.java
@@ -1,6 +1,8 @@
 package attendance;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import attendance.dto.FileRequestDto;
@@ -13,14 +15,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FileParserTest {
 
     @Test
-    @DisplayName("파일을 읽고 dto로 반환한다")
+    @DisplayName("파일을 읽고 문자열로 반환한다")
     void readFile() {
         FileParser reader = new FileParser("src/test/java/resources/testAttendances.csv");
         List<FileRequestDto> expected = List.of(
-                new FileRequestDto("쿠키", LocalDateTime.of(2024,12,13,10,8)),
-                new FileRequestDto("빙봉", LocalDateTime.of(2024,12,13,10,7)),
-                new FileRequestDto("빙티", LocalDateTime.of(2024,12,13,10,7)),
-                new FileRequestDto("이든", LocalDateTime.of(2024,12,13,10,7))
+                new FileRequestDto("쿠키", LocalDate.of(2024,12,13), LocalTime.of(10,8)),
+                new FileRequestDto("빙봉", LocalDate.of(2024,12,13), LocalTime.of(10,7)),
+                new FileRequestDto("빙티", LocalDate.of(2024,12,13), LocalTime.of(10,7)),
+                new FileRequestDto("이든", LocalDate.of(2024,12,13), LocalTime.of(10,7))
         );
 
         assertThat(reader.read()).isEqualTo(expected);
