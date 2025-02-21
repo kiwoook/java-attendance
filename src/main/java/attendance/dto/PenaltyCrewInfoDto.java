bae28a0132bf19254d913c9068e1924c81f34845
7i.w__k <tizmfns1218@Naver.com>
2025-02-21 20:03:52 +0900

    refactor: penaltyCrew 변수 변경

--- src/main/java/attendance/dto/PenaltyCrewInfoDto.java
+++ src/main/java/attendance/dto/PenaltyCrewInfoDto.java
@@ -6,7 +6,6 @@ import attendance.domain.PenaltyCrew;
 public record PenaltyCrewInfoDto(String name, int absenceCount, int lateCount, String penaltyMessage) {
 
     public static PenaltyCrewInfoDto toDto(PenaltyCrew penaltyCrew, int absenceCount, int lateCount) {
-
         return new PenaltyCrewInfoDto(penaltyCrew.getName(), absenceCount, lateCount,
                 AttendancePenalty.find(absenceCount, lateCount).getMessage());
     }
