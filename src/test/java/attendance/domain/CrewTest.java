578a8b15ea6dca229dd10398bba0525ea8644eed
7i.w__k <tizmfns1218@Naver.com>
2025-02-24 16:49:41 +0900

    feat: crew 기능 구현

--- src/test/java/attendance/domain/CrewTest.java
+++ src/test/java/attendance/domain/CrewTest.java
@@ -1,4 +1,163 @@
 package attendance.domain;
 
-public class CrewTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import attendance.common.ErrorMessage;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CrewTest {
+
+    // 특정 날짜(today)를 기준으로 시작날짜부터 전날까지의 List를 받아와야 한다.
+    // 해당 Map을 정렬해야 하며, AttendanceStatus, PenaltyStatus를 알 수 있다.
+
+    @DisplayName("이름으로 객체를 생성한다.")
+    @Test
+    void test1() {
+        String name = "꾹이";
+        LocalDate now = LocalDate.now();
+        assertThatCode(() -> Crew.from(name, now))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("출석을 추가할 수 있다.")
+    @Test
+    void addAttendanceTest() {
+        String crewName = "꾹이";
+        LocalDate today = LocalDate.of(2024, 12, 6);
+
+        Crew crew = Crew.from(crewName, today);
+        LocalDate localDate = LocalDate.of(2024, 12, 2);
+        LocalTime localTime = LocalTime.of(10, 0);
+        Map<LocalDate, Attendance> map = new HashMap<>();
+
+        Attendance attendance = new Attendance(localDate, localTime);
+        map.put(localDate, attendance);
+        Crew expect = new Crew(crewName, map, today);
+
+        crew.addAttendance(localDate, attendance);
+
+        assertThat(crew).isEqualTo(expect);
+    }
+
+    @DisplayName("똑같은 출석이 존재하면 에러를 반환한다.")
+    @Test
+    void returnErrorByAddAlreadyAttendance() {
+        String crewName = "꾹이";
+        LocalDate today = LocalDate.of(2024, 12, 6);
+
+        Crew crew = Crew.from(crewName, today);
+        LocalDate localDate = LocalDate.of(2024, 12, 2);
+        LocalTime localTime = LocalTime.of(10, 0);
+
+        Attendance attendance = new Attendance(localDate, localTime);
+        crew.addAttendance(localDate, attendance);
+
+        assertThatThrownBy(() -> crew.addAttendance(localDate, attendance))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ALREADY_ATTENDANCE.getMessage());
+    }
+
+    @DisplayName("출석 날짜의 출석 시간을 받아온다.")
+    @Test
+    void getAttendanceTimeByDateTest() {
+        String crewName = "꾹이";
+        LocalDate today = LocalDate.of(2024, 12, 6);
+
+        Crew crew = Crew.from(crewName, today);
+        LocalDate localDate = LocalDate.of(2024, 12, 2);
+        LocalTime localTime = LocalTime.of(10, 0);
+
+        Attendance attendance = new Attendance(localDate, localTime);
+        crew.addAttendance(localDate, attendance);
+
+        assertThat(crew.getAttendanceTimeByDate(localDate)).isEqualTo(localTime);
+
+    }
+
+    @DisplayName("출석 정보 없이 출석 날짜를 찾으면 에러를 발생한다.")
+    @Test
+    void getAttendanceTimeByDateTest2() {
+        String crewName = "꾹이";
+        LocalDate today = LocalDate.of(2024, 12, 6);
+
+        Crew crew = Crew.from(crewName, today);
+        LocalDate localDate = LocalDate.of(2024, 12, 2);
+
+        assertThatThrownBy(() -> crew.getAttendanceTimeByDate(localDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NOT_EXIST_ATTENDANCE.getMessage());
+    }
+
+    @DisplayName("출석을 수정할 수 있다.")
+    @Test
+    void editAttendanceTest() {
+        String crewName = "꾹이";
+        LocalDate today = LocalDate.of(2024, 12, 6);
+
+        Crew crew = Crew.from(crewName, today);
+        LocalDate localDate = LocalDate.of(2024, 12, 2);
+        LocalTime localTime = LocalTime.of(10, 0);
+        Map<LocalDate, Attendance> map = new HashMap<>();
+
+        Attendance attendance = new Attendance(localDate, localTime);
+        map.put(localDate, attendance);
+        Crew expect = new Crew(crewName, map, today);
+
+        crew.addAttendance(localDate, attendance);
+
+        assertThat(crew).isEqualTo(expect);
+    }
+
+    @DisplayName("기준 날짜를 받아와 AttendceStats를 받아온다.")
+    @Test
+    void getAttendanceStatsByDateTest() {
+        String crewName = "꾹이";
+        LocalDate today = LocalDate.of(2024, 12, 10);
+
+        Crew crew = Crew.from(crewName, today);
+        List<Integer> days = List.of(2, 3, 4, 5, 6, 9);
+
+        for (int day : days) {
+            LocalDate localDate = LocalDate.of(2024, 12, day);
+            LocalTime localTime = LocalTime.of(14, 0);
+
+            Attendance attendance = new Attendance(localDate, localTime);
+
+            crew.addAttendance(localDate, attendance);
+        }
+
+        assertThat(crew.getAttendanceStatsCountByDate(today)).isEqualTo(List.of(0, 0, 6));
+
+    }
+
+    @DisplayName("기준 날짜를 받아와 PenaltyStatus를 받아온다.")
+    @Test
+    void getPenaltyStatusByDateTest() {
+        String crewName = "꾹이";
+        LocalDate today = LocalDate.of(2024, 12, 10);
+
+        Crew crew = Crew.from(crewName, today);
+        List<Integer> days = List.of(2, 3, 4, 5, 6, 9);
+
+        for (int day : days) {
+            LocalDate localDate = LocalDate.of(2024, 12, day);
+            LocalTime localTime = LocalTime.of(14, 0);
+
+            Attendance attendance = new Attendance(localDate, localTime);
+
+            crew.addAttendance(localDate, attendance);
+        }
+
+        assertThat(crew.getPenaltyStatusByDate(today)).isEqualTo(PenaltyStatus.EXPULSION);
+    }
+
+
 }
