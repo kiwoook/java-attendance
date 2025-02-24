ad0057739176f74e2f13d84794bdb2d75be6205f
7i.w__k <tizmfns1218@Naver.com>
2025-02-24 14:23:14 +0900

    feat: attendance 기능 구현

---
+++ src/main/java/attendance/domain/Attendance.java
@@ -0,0 +1,67 @@
+package attendance.domain;
+
+import static attendance.common.ErrorMessage.INVALID_ATTENDANCE_DAY;
+import static attendance.common.ErrorMessage.INVALID_ATTENDANCE_TIME;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+public class Attendance {
+
+    private final LocalDate attendanceDate;
+    private LocalTime attendanceTime;
+
+    public Attendance(LocalDate attendanceDate, LocalTime attendanceTime) {
+        validateOpenDay(attendanceDate);
+        validateOpenTime(attendanceTime);
+        this.attendanceDate = attendanceDate;
+        this.attendanceTime = attendanceTime;
+    }
+
+    private static void validateOpenDay(LocalDate attendanceDate) {
+        DayOfWeek dayOfWeek = attendanceDate.getDayOfWeek();
+
+        if (attendanceDate.getDayOfMonth() == 25) {
+            throw new IllegalArgumentException(INVALID_ATTENDANCE_DAY.getMessage());
+        }
+
+        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+            throw new IllegalArgumentException(INVALID_ATTENDANCE_DAY.getMessage());
+        }
+    }
+
+    private static void validateOpenTime(LocalTime attendanceTime) {
+        if (attendanceTime.isAfter(LocalTime.of(23, 0)) || attendanceTime.isBefore(LocalTime.of(8, 0))) {
+            throw new IllegalArgumentException(INVALID_ATTENDANCE_TIME.getMessage());
+        }
+    }
+
+    public boolean isLocalDate(LocalDate localDate) {
+        return attendanceDate.equals(localDate);
+    }
+
+    public void editTime(LocalTime editTime) {
+        this.attendanceTime = editTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Attendance that = (Attendance) o;
+        return Objects.equals(attendanceDate, that.attendanceDate) && Objects.equals(attendanceTime,
+                that.attendanceTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attendanceDate, attendanceTime);
+    }
+
+    public AttendanceStatus getStatus() {
+        return AttendanceStatus.of(attendanceDate, attendanceTime);
+    }
+}
--- src/test/java/attendance/domain/AttendanceTest.java
+++ src/test/java/attendance/domain/AttendanceTest.java
@@ -1,14 +1,85 @@
 package attendance.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import attendance.common.ErrorMessage;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AttendanceTest {
 
     @DisplayName("날짜와 시간을 입력받아 출석을 정의한다.")
     @Test
-    void test1(){
+    void test1() {
+        LocalDate localDate = LocalDate.of(2024, 12, 16);
+        LocalTime localTime = LocalTime.of(10, 0);
+
+        assertThatCode(() -> new Attendance(localDate, localTime))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("운영하지 않는 날짜에 대한 생성은 에러가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {14, 15, 25})
+    void test2(int day) {
+        LocalDate notOpenDay = LocalDate.of(2024, 12, day);
+        LocalTime localTime = LocalTime.of(10, 0);
+
+        assertThatCode(() -> new Attendance(notOpenDay, localTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_ATTENDANCE_DAY.getMessage());
+    }
+
+    @DisplayName("해당 출석 날짜이면 true를 반환한다")
+    @Test
+    void test3() {
+        LocalDate localDate = LocalDate.of(2024, 12, 16);
+        LocalTime localTime = LocalTime.of(10, 0);
+        Attendance attendance = new Attendance(localDate, localTime);
+
+        assertThat(attendance.isLocalDate(localDate)).isTrue();
 
     }
+
+    @DisplayName("해당 출석 날짜가 아니면 false를 반환한다.")
+    @Test
+    void test4() {
+        LocalDate localDate = LocalDate.of(2024, 12, 16);
+        LocalTime localTime = LocalTime.of(10, 0);
+        Attendance attendance = new Attendance(localDate, localTime);
+
+        assertThat(attendance.isLocalDate(localDate.plusDays(1))).isFalse();
+    }
+
+    @DisplayName("출석 시간을 수정할 수 있다.")
+    @Test
+    void test5() {
+        LocalDate localDate = LocalDate.of(2024, 12, 16);
+        LocalTime localTime = LocalTime.of(10, 0);
+        Attendance attendance = new Attendance(localDate, localTime);
+
+        LocalTime editTime = LocalTime.of(11, 0);
+        Attendance expect = new Attendance(localDate, editTime);
+
+        attendance.editTime(editTime);
+
+        assertThat(attendance).isEqualTo(expect);
+    }
+
+    @DisplayName("출석, 지각, 결석 여부를 알 수 있다.")
+    @Test
+    void test6() {
+        LocalDate localDate = LocalDate.of(2024, 12, 16);
+        LocalTime localTime = LocalTime.of(10, 0);
+        Attendance attendance = new Attendance(localDate, localTime);
+
+        assertThat(attendance.getStatus()).isEqualTo(AttendanceStatus.PRESENCE);
+    }
+
 
 }
