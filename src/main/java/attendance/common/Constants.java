242b412146158b2106a063e2a5c49cc94d623c36
7i.w__k <tizmfns1218@Naver.com>
2025-02-24 23:18:57 +0900

    feat: 최대 날짜 기능 추가
    - 1월이 넘어도 12월까지의 데이터만 처리할 수 있도록 변경

--- src/main/java/attendance/common/Constants.java
+++ src/main/java/attendance/common/Constants.java
@@ -4,7 +4,8 @@ import java.time.LocalDate;
 
 public final class Constants {
 
-    public static final LocalDate START_DAY = LocalDate.of(2024, 12, 2);
+    public static final LocalDate START_DATE = LocalDate.of(2024, 12, 2);
+    public static final LocalDate END_DATE = LocalDate.of(2024, 12, 31);
 
     private Constants() {
     }
